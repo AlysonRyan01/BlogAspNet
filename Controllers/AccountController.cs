259030dfa99df69a451460206663dd607dd50c99
AlysonRyan01 <sdhuahdsaih@gmail.com>
2024-12-03 17:30:26 -0300

    .

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 using BlogAspNet.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,11 +8,24 @@ namespace BlogAspNet.Controllers;
 [ApiController]
 public class AccountController : ControllerBase
 {
-    [HttpPost("v1/login")]
+    [HttpGet("v1/login")]
     public IActionResult Login([FromServices] TokenService tokenService)
     {
         var token = tokenService.GenerateToken(null);
 
         return Ok(token);
     }
+    
+    [Authorize(Roles = "user, admin")]
+    [HttpGet("v1/User")]
+    public IActionResult GetUser() => Ok(User.Identity.Name);
+    
+    [Authorize(Roles = "author, admin")]
+    [HttpGet("v1/Author")]
+    public IActionResult GetAuthor() => Ok(User.Identity.Name);
+    
+    [Authorize(Roles = "admin")]
+    [HttpGet("v1/Admin")]
+    public IActionResult GetAdmin() => Ok(User.Identity.Name);
+    
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,28 @@
+using System.Text;
+using BlogAspNet;
 using BlogAspNet.Data;
 using BlogAspNet.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+
 builder.Services
     .AddControllers()
     .ConfigureApiBehaviorOptions(options =>
@@ -13,6 +34,10 @@ builder.Services.AddDbContext<BlogDataContext>();
 builder.Services.AddTransient<TokenService>(); //* Sempre criar um novo tokenService
 
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
